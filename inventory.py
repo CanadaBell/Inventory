724a084e7a463fbbc092a3137d1ff09e8e47d4f6
CanadaBell <123008017+CanadaBell@users.noreply.github.com>
2023-05-04 13:41:53 -0600

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,4 +1,5 @@
 # Inputs
+import random as r
 import os
 import time
 import difflib
@@ -7,9 +8,9 @@ from textwrap import dedent
 
 inventory = {} # Empty dictionary that will be filled later
 inventory2 = {} # Empty dictionary that will be filled later
-inventory3 = {} # Empty dictionary that will be filled later
 
 ### Menu Functions ###
+
 ## Main Menu ##
 def main_menu():
     os.system('cls' if os.name =='nt' else 'clear')
@@ -52,11 +53,14 @@ def first_menu():
 
     Create a whole new inventory
 
+    You can also pick a random inventory
+
     Please enter one of the following commands: 
     1. Upload File 
     2. Create File
-    3. View Inventory
-    4. Go Back
+    3. Random Inventory
+    4. View Inventory
+    5. Go Back
 
     """))
     command = input("> ")
@@ -64,7 +68,7 @@ def first_menu():
     while True:
         try:
             command = int(command)
-            if command not in [1, 2, 3, 4]:
+            if command not in [1, 2, 3, 4, 5]:
                 command = input("Please input a valid command ")
                 continue
             break
@@ -150,6 +154,80 @@ def returning():
         t += 0.5
     return
 
+## Random Inventory ##
+def random_inv():
+    global inventory
+    os.system('cls' if os.name =='nt' else 'clear')
+    pick = r.randint(1, 5)
+    if pick == 1:
+        inventory = {
+            "Self-cleaning litter box": [0, 249.99],
+            "Magnetic cat door": [0, 36.50],
+            "Cute cat-shaped salt and pepper shakers": [0, 9.99],
+            "Feather wand toy": [0, 7.50],
+            "Elevated food and water bowls": [0, 29.99],
+            "Cute cat figurine": [0, 7.50],
+            "Scratch post": [0, 29.95],
+            "Stylish cat carrier purse": [0, 58.99],
+            "Grooming gloves": [0, 14.95],
+            "Luxury cat bed": [0, 89.99],}
+            
+    elif pick == 2:
+        inventory = {
+            "Poop Bags": [0, 8.85],
+            "Dog Raincoat": [0, 34.99],
+            "Dog Harness": [0, 26.99],
+            "Dog Stuffed Animal": [0, 12.99],
+            "Grooming Brush": [0, 11.75],
+            "Dog Boots": [0, 24.99],
+            "Dog Pajamas": [0, 15.99],
+            "Dog GPS Tracker": [0, 79.99],
+            "Dog Christmas Sweater": [0, 18.99],
+            "Dog Carrier Backpack": [0, 48.99],}
+
+    elif pick == 3:
+        inventory = {
+            "Gaming Mouse": [0,29.99],
+            "Tote bag": [0,19.50],
+            "Plant": [0,14.99],
+            "Desk Lamp": [0,22.75],
+            "Hair Dryer": [0,34.50],
+            "Sneakers": [0,89.99],
+            "Nail Polish": [0,6.25],
+            "Coffee Mug": [0,8.50],
+            "Candle": [0,9.99],
+            "Wi-Fi Extender": [0,27.50],}
+    
+    elif pick == 4:
+        inventory = {
+            "Backpack": [0,34.99],
+            "Picture Frame": [0,7.99],
+            "Desk Chair": [0,79.99],
+            "Yoga Mat": [0,24.72],
+            "Water Bottle": [0,12.99],
+            "Sunglasses": [0,24.95],
+            "Card Game": [0,14.95],
+            "T-shirt": [0,19.99],
+            "Notebook": [0,4.99],
+            "Watering Can": [0,11.25],}
+    
+    else:
+        inventory = {
+            "Shower Curtain": [0,15.99],
+            "Lip Balm": [0,3.49],
+            "Jump Rope": [0,8.55],
+            "Cooking Timer": [0,7.50],
+            "Speaker": [0,39.50],
+            "Android Phone Charger": [0,9.99],
+            "Bath Mat": [0,16.25],
+            "Yoga Block": [0,10.99],
+            "Umbrella": [0,16.75],
+            "Fishing Rod": [0,59.99],}
+
+    print ("Random Inventory Selected")
+    returning()
+    return
+
 ## File Reader ##
 def file_reader():
     global inventory, inventory2, inventory3
@@ -186,7 +264,6 @@ def file_reader():
     os.system('cls' if os.name =='nt' else 'clear')
     returning()
     return
-
 ## Inventory Printer ##
 def inventory_print(num_of_inv):
     
@@ -199,20 +276,15 @@ def inventory_print(num_of_inv):
             print ("If nothing prints then the invintory is empty")
         for p,i in inventory.items():
             print (f"{p}: {i[0]}, {i[1]}")
-    elif num_of_inv == 2:
+    else:
         if len(inventory2) == 0:
             print ("If nothing prints then the invintory is empty")
         for p,i in inventory2.items():
             print (f"{p}: {i[0]}, {i[1]}")
-    else:
-        if len(inventory2) == 0:
-            print ("If nothing prints then the invintory is empty")
-        for p,i in inventory3.items():
-            print (f"{p}: {i[0]}, {i[1]}")
     time.sleep(5)
 
     os.system('cls' if os.name =='nt' else 'clear')
-    t = 0
+
     returning()
     return
 
@@ -294,7 +366,8 @@ def addinventory(): # Function for user to add inv (fills dict)
 ### Second Function (Adding Stock) ###
 def addstock():
     global inventory, inventory2
-    inventory2 = inventory.copy()
+    if len(inventory2) == 0:
+        inventory2 = inventory.copy()
     os.system('cls' if os.name =='nt' else 'clear')
 
     ## Getting Stock ##
@@ -324,8 +397,7 @@ def addstock():
 ### Third Function (Taking Orders) ###
 def orders():
     while True:
-        global inventory2, inventory3
-        inventory3 = inventory2.copy()
+        global inventory2
         os.system('cls' if os.name =='nt' else 'clear')
 
         ## Getting Items for Order ##
@@ -402,12 +474,15 @@ def orders():
                 except ValueError:
                     amount = input(f"NaN, Please try again: ")
 
-            # Writing to File #
-            inventory3[product] = [inventory3[product][0] - amount, inventory3[product][1]]
+            # Upadting Dict #
+            inventory2[product] = [inventory2[product][0] - amount, inventory2[product][1]]
 
-            file.write(f"\n{inventory3[product]}|{inventory3[product][0]}|{inventory3[product][1]}")
+        for prod, info in inventory2.items():
+            file.write(f"\n{prod}| {info[0]} |{info[1]}")
 
         file.close()
+
+        os.system('cls' if os.name =='nt' else 'clear')
         ask = input("Is there another order you would like to enter (Y/N): ").lower()
         if ask == "n":
             os.system('cls' if os.name =='nt' else 'clear')
@@ -416,6 +491,7 @@ def orders():
     return
 
 ### Outputs ###
+
 while True:
     dec = main_menu()
 
@@ -427,8 +503,11 @@ while True:
                 continue
             elif dec == 2:
                 addinventory()
-                continue
+                break
             elif dec == 3:
+                random_inv()
+                continue
+            elif dec == 4:
                 inventory_print(1)
                 continue
             else:
@@ -439,7 +518,7 @@ while True:
             dec = second_menu()
             if dec == 1:
                 addstock()
-                continue
+                break
             elif dec == 2:
                 inventory_print(2)
                 continue
@@ -451,9 +530,9 @@ while True:
             dec = third_menu()
             if dec == 1:
                 orders()
-                continue
+                break
             elif dec == 2:
-                inventory_print(3)
+                inventory_print(2)
                 continue
             else:
                 break
@@ -462,6 +541,3 @@ while True:
         os.system('cls' if os.name =='nt' else 'clear')
         print("Thank you for using Bamazon Inventory System Solutions\nHope to see you again")
         sys.exit()
-
-# addstock()
-# orders()
